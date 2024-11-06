8a06114cde80f8efab79deb8e407a3311b7c3939
DivineDris <sava200104@gmail.com>
2024-11-06 21:17:22 +0300

    Code added

---
+++ ProductViewerApp/ProductViewerApp/Pages/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ProductViewerApp.Models;
+
+namespace ProductViewerApp.Pages
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Product()
+
+        {
+
+            var products = GetProducts();
+
+            return View(products);
+
+        }
+
+        private List<Product> GetProducts()
+
+        {
+
+            var json = System.IO.File.ReadAllText("wwwroot/products.json");
+
+            return JsonConvert.DeserializeObject<List<Product>>(json);
+
+        }
+    }
+}
--- ProductViewerApp/ProductViewerApp/obj/Debug/net8.0/ProductViewerApp.AssemblyInfo.cs
+++ ProductViewerApp/ProductViewerApp/obj/Debug/net8.0/ProductViewerApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductViewerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a07456d204d282ee2a1a2f1e7962f16d3325d35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f36e96573f3876be09d17359ca6fa53f023049d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductViewerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductViewerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
